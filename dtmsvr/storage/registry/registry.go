c9280fefd1cbadd506d96c2f7f13e6954ad3c328
yedf2 <120050102@qq.com>
2022-01-07 19:55:15 +0800

    store refactor

--- dtmsvr/storage/registry/registry.go
+++ dtmsvr/storage/registry/registry.go
@@ -1,6 +1,7 @@
 package registry
 
 import (
+	"sync"
 	"time"
 
 	"github.com/dtm-labs/dtm/dtmsvr/config"
@@ -12,16 +13,42 @@ import (
 
 var conf = &config.Config
 
-var stores map[string]storage.Store = map[string]storage.Store{
-	"redis":    &redis.Store{},
-	"mysql":    &sql.Store{},
-	"postgres": &sql.Store{},
-	"boltdb":   &boltdb.Store{},
+type storeFactory struct {
+	once    sync.Once
+	store   storage.Store
+	factory func() storage.Store
+}
+
+var factories map[string]*storeFactory = map[string]*storeFactory{
+	"redis": {
+		factory: func() storage.Store {
+			return &redis.Store{}
+		},
+	},
+	"mysql": {
+		factory: func() storage.Store {
+			return &sql.Store{}
+		},
+	},
+	"postgres": {
+		factory: func() storage.Store {
+			return &sql.Store{}
+		},
+	},
+	"boltdb": {
+		factory: func() storage.Store {
+			return &boltdb.Store{}
+		},
+	},
 }
 
 // GetStore returns storage.Store
 func GetStore() storage.Store {
-	return stores[conf.Store.Driver]
+	f := factories[conf.Store.Driver]
+	f.once.Do(func() {
+		f.store = f.factory()
+	})
+	return f.store
 }
 
 // WaitStoreUp wait for db to go up
